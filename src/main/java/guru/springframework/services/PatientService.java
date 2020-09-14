7ed74c62a81a4b6489d4948fdbd34d61eb020e93
AnamikaGautam52 <annu436gautam@gmail.com>
2020-09-14 13:09:35 +0000

    service

---
+++ src/main/java/guru/springframework/services/PatientService.java
@@ -0,0 +1,22 @@
+package guru.springframework.services;
+
+import guru.springframework.commands.PatientForm;
+import guru.springframework.domain.Patient;
+
+import java.util.List;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+public interface PatientService {
+
+    List<Patient> listAll();
+
+    Patient getById(String id);
+
+    Patient saveOrUpdate(Patient patient);
+
+    void delete(String id);
+
+    Patient saveOrUpdateProductForm(PatientForm patientForm);
+}
