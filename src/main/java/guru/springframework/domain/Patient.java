ae5bb653f713caec3b39d984dfeb4cc009ee813e
AnamikaGautam52 <annu436gautam@gmail.com>
2020-09-14 13:05:08 +0000

    repo

---
+++ src/main/java/guru/springframework/domain/Patient.java
@@ -0,0 +1,51 @@
+package guru.springframework.domain;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by jt on 1/10/17.
+ */
+@Document
+public class Patient {
+    @Id
+    private ObjectId _id;
+    private String Name;
+    private BigDecimal Age;
+    private String Gender;
+
+    public ObjectId getId() {
+        return _id;
+    }
+
+    public void setId(ObjectId id) {
+        this._id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public BigDecimal getAge() {
+        return Age;
+    }
+
+    public void setAge(BigDecimal Age) {
+        this.Age = Age;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+}
